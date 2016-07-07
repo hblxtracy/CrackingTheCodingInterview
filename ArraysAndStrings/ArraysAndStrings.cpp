81b770de07f121986d695b013f08c7f7f6254251
Li, Xin (US) <xin.li3@elekta.com>
2016-07-06 17:53:50 -0700

    Initial checkin

---
+++ ArraysAndStrings/ArraysAndStrings.cpp
@@ -0,0 +1,58 @@
+// ArraysAndStrings.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+//1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters.What if you can not use additional data structures ?
+
+
+//1.2 Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+
+//1.3 Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer. NOTE: One or two additional variables are fine. An extra copy of the array is not.
+//FOLLOW UP
+//Write the test cases for this method.
+
+
+//1.4 Check Permutation: Write a method to decide if two strings are anagrams or not.
+
+//1.5 URLify: Write a method to replace all spaces in a string with ‘%20’. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string.
+
+//1.6 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+//1.7 Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.
+
+//1.8 String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (i.e., “waterbottle” is a rotation of “erbottlewat”).
+
+//1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+//A palindrome is a word or phrase that is the same forwards and backwards.A permutation
+//is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words.
+//EXAMPLE
+//Input : Tact Coa
+//	Output : True(permutations : "taco cat", "atco eta", etc.)
+
+//1.5 One Away: There are three types of edits that can be performed on strings: insert a character,
+//remove a character, or replace a character.Given two strings, write a function to check if they are
+//one edit(or zero edits) away.
+//EXAMPLE
+//pale, ple -> true
+//pales, pale -> true
+//pale, bale -> true
+//pale, bake -> false
+
+//1.6 String Compression: Implement a method to perform basic string compression using the counts
+//of repeated characters.For example, the string aabcccccaaa would become a2blc5a3.If the
+//"compressed" string would not become smaller than the original string, your method should return
+//the original string.You can assume the string has only uppercase and lowercase letters(a - z).
+int main()
+{
+	string InputString = "Hello";
+	//string InputString = "Abcdef ghijk";
+
+
+
+
+	return 0;
+}
+
