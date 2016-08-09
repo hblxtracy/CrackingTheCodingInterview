5380a9dbc43f78c3ad671491e5942d76fea76d94
Li, Xin (US) <xin.li3@elekta.com>
2016-08-09 14:44:28 -0700

    Added functions for TR tree.

--- TreesAndGraphs/RB.cpp
+++ TreesAndGraphs/RB.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "RB.h"
+#include <iostream>
 
+using namespace std;
 
 RB::RB()
 {
@@ -21,4 +23,70 @@ void RB::ClearTree(Node* t)
 		ClearTree(t->right);
 	delete t;
 	return;
+}
+
+bool RB::IsRed(Node* t)
+{
+	if (t == NULL)
+		return false;
+	return t->red;
+}
+
+void RB::ChangeColor(Node* &t)
+{
+	if (IsRed(t))
+		t->red = false;
+	else
+		t->red = true;
+}
+
+RB::Node* RB::U(Node* &t)
+{
+	if (t->parent == NULL)
+	{
+		cout << "Node does not have parent.";
+		return NULL;
+	}
+	if (t->parent->parent == NULL)
+	{
+		cout << "Node does not have grandparent.";
+		return NULL;
+	}
+	if (t->parent == t->parent->parent->left)
+		return t->parent->parent->right;
+	else
+		return t->parent->parent->left;
+}
+
+RB::Node* RB::S(Node* &t)
+{
+	if (t->parent == NULL)
+	{
+		cout << "Node does not have parent.";
+			return NULL;
+	}
+	if (t = t->parent->left)
+		return t->parent->right;
+	else
+		return t->parent->left;
+}
+
+RB::Node* RB::G(Node* &t)
+{
+	if (t->parent == NULL)
+	{
+		cout << "Node does not have parent.";
+		return NULL;
+	}
+	if (t->parent->parent == NULL)
+	{
+		cout << "Node does not have grandparent.";
+		return NULL;
+	}
+	return t->parent->parent;
+}
+
+RB::Node* RB::P(Node* &t)
+{
+	return t->parent;
 }
--- TreesAndGraphs/RB.h
+++ TreesAndGraphs/RB.h
@@ -18,5 +18,11 @@ public:
 	void Insert(Node* &t, int key);
 	void RightRotation(Node* &t);
 	void LeftRotation(Node* &t);
+	bool IsRed(Node *t);
+	void ChangeColor(Node* &t);
+	Node* U(Node* &t);
+	Node* S(Node* &t);
+	Node* G(Node* &t);
+	Node* P(Node* &t);
 };
 
